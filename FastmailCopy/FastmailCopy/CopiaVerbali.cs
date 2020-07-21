9be05630b788adc7891895a7152bccb3fd873d64
Andrea Di Gianvittorio <andrea.digianvittorio@outlook.com>
2020-07-22 00:07:39 +0200

    3

--- FastmailCopy/FastmailCopy/CopiaVerbali.cs
+++ FastmailCopy/FastmailCopy/CopiaVerbali.cs
@@ -13,8 +13,12 @@ namespace FastmailCopy
     {
         public void copy(String SPath, String TPath)
         {
-            DirectoryInfo dir = new DirectoryInfo(SPath);
-            FileInfo[] files = dir.GetFiles();
+            DirectoryInfo SDir = new DirectoryInfo(SPath);
+            DirectoryInfo TDir = new DirectoryInfo(Path.Combine(TPath, "TEST"));
+            TDir.Create();
+
+            TPath = Path.Combine(TPath, TDir.Name);
+            FileInfo[] files = SDir.GetFiles();
             foreach(FileInfo file in files)
             {
                 string TempPath = Path.Combine(TPath, file.Name);
